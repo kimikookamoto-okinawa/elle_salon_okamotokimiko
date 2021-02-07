950c21fec4102b6d646df92fa5986aa46925474c
kimikookamoto <miyazato218@gmail.com>
2021-02-08 07:27:20 +0900

    Initial Commit

---
+++ js/common.js
@@ -0,0 +1,13 @@
+$('.slick01').slick({ //{}を入れる
+    autoplay: true,
+    autoplaySpeed: 5000,
+    arrows: true,
+    slidesToShow: 3,
+    centerMode: true,
+    centerPadding: '0%',
+    focusOnSelect: true,
+    infinite : true,
+    pauseOnHover: false,
+    prevArrow: '<img src="./left.png" class="slide-arrow prev-arrow">',
+    nextArrow: 'none'
+  });
